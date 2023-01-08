9cdf4cd8fb4b2abf274ae1032383404f7f25f0c1
Semen Alekseev <semyon492@ya.ru>
2023-01-08 20:05:41 +0000

    License

--- src/Exception/InvalidIdentifier.php
+++ src/Exception/InvalidIdentifier.php
@@ -1,5 +1,14 @@
 <?php
-namespace Tephida\Database\Exception;
+
+/*
+ * Copyright (c) 2023 Sura
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *   file that was distributed with this source code.
+ *
+ */
+
+ namespace Tephida\Database\Exception;
 
 use Tephida\Corner\CornerTrait;
 
