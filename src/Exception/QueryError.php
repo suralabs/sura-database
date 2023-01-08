6a252e660ffc0113bf92b12ea1ccdebdba95ef24
Semen Alekseev <semyon492@ya.ru>
2023-01-08 23:11:35 +0300

    License update

--- src/Exception/QueryError.php
+++ src/Exception/QueryError.php
@@ -1,4 +1,13 @@
 <?php
+
+/*
+ * Copyright (c) 2023 Sura
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *   file that was distributed with this source code.
+ *
+ */
+
 namespace Tephida\Database\Exception;
 
 use Tephida\Corner\CornerTrait;
